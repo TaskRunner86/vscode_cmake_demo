cd31c202f67018f23cd5b0b223929214cd27e7b8
vv <gaffer.c@foxmail.com>
2020-07-23 15:25:52 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+# include "add.h"
+
+
+int main(int argc, const char** argv) {
+	int a = 0;
+	int b = 1;
+	int c = add(a, b);
+
+#ifdef UU
+	printf("UU\n");
+#endif // UU
+
+	return 0;
+}
